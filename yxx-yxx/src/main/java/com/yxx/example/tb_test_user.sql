02db065f8a63fe42dfbd1764b6b8704593bdd044
yxx7318 <1303490776@qq.com>
2025-02-08 14:15:02 +0800

    2025-02-08：优化代码测试表建表sql，修复在前端界面使用是存在sql注入的异常

--- yxx-yxx/src/main/java/com/yxx/example/tb_test_user.sql
+++ yxx-yxx/src/main/java/com/yxx/example/tb_test_user.sql
@@ -1,10 +1,3 @@
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for tb_test_user
--- ----------------------------
-DROP TABLE IF EXISTS `tb_test_user`;
 CREATE TABLE `tb_test_user`  (
   `userId` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名称',
@@ -15,7 +8,5 @@ CREATE TABLE `tb_test_user`  (
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   `update_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '更新人',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`userId`) USING BTREE
+  PRIMARY KEY (`userId`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '代码生成测试表' ROW_FORMAT = DYNAMIC;
-
-SET FOREIGN_KEY_CHECKS = 1;
