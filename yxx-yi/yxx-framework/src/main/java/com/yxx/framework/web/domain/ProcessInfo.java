665a1fe1c7fb3d730336d51f25e97eef84e9be76
yxx7318 <1303490776@qq.com>
2025-02-13 15:03:30 +0800

    2025-02-13：新增应用程序监控后端代码

---
+++ yxx-yi/yxx-framework/src/main/java/com/yxx/framework/web/domain/ProcessInfo.java
@@ -0,0 +1,137 @@
+package com.yxx.framework.web.domain;
+
+import java.io.Serializable;
+
+public class ProcessInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 进程 ID，唯一标识一个进程
+     */
+    private int pid;
+
+    /**
+     * 进程名称，通常是可执行文件的名称或服务名称
+     */
+    private String name;
+
+    /**
+     * 进程的命令行参数，包含启动该进程时使用的完整命令及其参数
+     */
+    private String commandLine;
+
+    /**
+     * 进程的内存使用情况，通常表示为当前进程占用的物理内存大小
+     */
+    private String memoryUsage;
+
+    /**
+     * 进程的 CPU 使用率，表示该进程占用了多少 CPU 资源
+     */
+    private double cpuUsagePercent;
+
+    /**
+     * 进程的启动时间，表示该进程开始运行的时间"yyyy-MM-dd HH:mm:ss"
+     */
+    private String startTime;
+
+    /**
+     * 运行该进程的用户名称
+     */
+    private String user;
+
+    /**
+     * 进程的状态，描述当前进程的运行状态
+     * 常见的状态包括：
+     * - "Running"：正在运行
+     * - "Sleeping"：休眠
+     * - "Stopped"：已停止
+     * - "Zombie"：僵尸进程
+     */
+    private String state;
+
+    /**
+     * 进程监听信息
+     */
+    private String listening;
+
+    @Override
+    public String toString() {
+        return String.format(
+                "PID: %d\nName: %s\nCommand Line: %s\nMemory: %s\nCPU Usage: %.1f%%\nStart Time: %s\nUser: %s\nState: %s\nListening: %s\n",
+                pid, name, commandLine, memoryUsage, cpuUsagePercent * 100, startTime, user, state, listening);
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCommandLine() {
+        return commandLine;
+    }
+
+    public void setCommandLine(String commandLine) {
+        this.commandLine = commandLine;
+    }
+
+    public String getMemoryUsage() {
+        return memoryUsage;
+    }
+
+    public void setMemoryUsage(String memoryUsage) {
+        this.memoryUsage = memoryUsage;
+    }
+
+    public double getCpuUsagePercent() {
+        return cpuUsagePercent;
+    }
+
+    public void setCpuUsagePercent(double cpuUsagePercent) {
+        this.cpuUsagePercent = cpuUsagePercent;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getListening() {
+        return listening;
+    }
+
+    public void setListening(String listening) {
+        this.listening = listening;
+    }
+}
---
+++ yxx-yi/yxx-framework/src/main/java/com/yxx/framework/yxx/utils/ProcessMonitorUtils.java
@@ -0,0 +1,153 @@
+package com.yxx.framework.yxx.utils;
+
+import com.yxx.framework.web.domain.ProcessInfo;
+import oshi.SystemInfo;
+import oshi.software.os.OSProcess;
+import oshi.software.os.OperatingSystem;
+import oshi.util.FormatUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class ProcessMonitorUtils {
+
+    /**
+     * 根据应用名称查找进程
+     */
+    public static List<ProcessInfo> findProcessesByAppName(String appName) {
+        SystemInfo si = new SystemInfo();
+        OperatingSystem os = si.getOperatingSystem();
+        List<ProcessInfo> result = new ArrayList<>();
+
+        for (OSProcess process : os.getProcesses()) {
+            String name = process.getName();
+            String commandLine = process.getCommandLine();
+
+            if ((name != null && name.contains(appName)) ||
+                    (commandLine != null && commandLine.contains(appName))) {
+                ProcessInfo info = createProcessInfo(process);
+                result.add(info);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 根据端口号查找进程
+     */
+    public static ProcessInfo findProcessByPort(int port) {
+        int pid = findPidByPort(port);
+        if (pid == -1) return null;
+
+        SystemInfo si = new SystemInfo();
+        OperatingSystem os = si.getOperatingSystem();
+        OSProcess process = os.getProcess(pid);
+
+        return createProcessInfo(process);
+    }
+
+    private static ProcessInfo createProcessInfo(OSProcess process) {
+        ProcessInfo info = new ProcessInfo();
+        info.setPid(process.getProcessID());
+        info.setName(process.getName());
+        info.setCommandLine(process.getCommandLine());
+        info.setMemoryUsage(FormatUtil.formatBytes(process.getResidentSetSize()));
+        info.setCpuUsagePercent(process.getProcessCpuLoadBetweenTicks(process));
+        info.setStartTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format((new Date(process.getStartTime()))));
+        info.setUser(process.getUser());
+        info.setState(process.getState().name());
+        info.setListening(getListeningByPid(info.getPid()));
+        return info;
+    }
+
+    /**
+     * 通过端口号获取PID
+     *
+     * @param port 端口
+     * @return PID
+     */
+    private static int findPidByPort(int port) {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            return findPidByPortWindows(port);
+        } else if (os.contains("nix") || os.contains("nux") || os.contains("mac")) {
+            return findPidByPortLinux(port);
+        }
+        throw new UnsupportedOperationException("Unsupported OS");
+    }
+
+    private static int findPidByPortWindows(int port) {
+        String command = "netstat -ano | findstr :" + port;
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec(new String[]{"cmd.exe", "/c", command});
+        } catch (IOException e) {
+            return -1;
+        }
+        try (
+                InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream(), "GBK");
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] tokens = line.trim().split("\\s+");
+                if (tokens.length >= 5 && tokens[1].contains(":" + port)) {
+                    return Integer.parseInt(tokens[4]);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return -1;
+    }
+
+    private static int findPidByPortLinux(int port) {
+        // 构造命令字符串
+        String command = "netstat -tulnp | grep \":%d\\b\" | awk '{print $7}' | cut -d'/' -f1";
+        String[] cmd = {"/bin/sh", "-c", String.format(command, port)};
+
+        // 执行命令
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec(cmd);
+        } catch (IOException e) {
+            return -1;
+        }
+        try (
+                InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream());
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+        ) {
+            String line = reader.readLine();
+            return line != null ? Integer.parseInt(line.trim()) : -1;
+        } catch (NumberFormatException | IOException e) {
+            return -1;
+        }
+    }
+
+    private static String getListeningByPid(int pid) {
+        try {
+            String os = System.getProperty("os.name").toLowerCase();
+            String[] cmd = os.contains("win")
+                    ? new String[]{"cmd", "/c", String.format("netstat -ano | findstr %d", pid)}
+                    : new String[]{"/bin/sh", "-c", String.format("netstat -tulnp | grep %d", pid)};
+            Process process = Runtime.getRuntime().exec(cmd);
+
+            StringBuilder ports = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), os.contains("win") ? "GBK" : "UTF-8"))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    ports.append(line).append("\n");
+                }
+            }
+            return ports.toString();
+        } catch (IOException e) {
+            return "N/A";
+        }
+    }
+}
