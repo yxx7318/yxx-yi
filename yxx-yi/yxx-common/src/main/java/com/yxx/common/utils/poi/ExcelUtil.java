410e2ed5b952d7e21e99ff61ee1164882ca34ca4
yxx7318 <1303490776@qq.com>
2025-03-02 18:27:20 +0800

    2025-03-02：合并若依主分支03-01:4df52a6，优化导出Excel日期格式双击离开后与设定的格式不一致问题

--- yxx-yi/yxx-common/src/main/java/com/yxx/common/utils/poi/ExcelUtil.java
+++ yxx-yi/yxx-common/src/main/java/com/yxx/common/utils/poi/ExcelUtil.java
@@ -1148,6 +1148,7 @@ public class ExcelUtil<T>
                 String dictType = attr.dictType();
                 if (StringUtils.isNotEmpty(dateFormat) && StringUtils.isNotNull(value))
                 {
+                    cell.getCellStyle().setDataFormat(this.wb.getCreationHelper().createDataFormat().getFormat(dateFormat));
                     cell.setCellValue(parseDateToStr(dateFormat, value));
                 }
                 else if (StringUtils.isNotEmpty(readConverterExp) && StringUtils.isNotNull(value))
@@ -1448,8 +1449,7 @@ public class ExcelUtil<T>
      */
     public String encodingFilename(String filename)
     {
-        filename = UUID.randomUUID() + "_" + filename + ".xlsx";
-        return filename;
+        return UUID.randomUUID() + "_" + filename + ".xlsx";
     }
 
     /**
