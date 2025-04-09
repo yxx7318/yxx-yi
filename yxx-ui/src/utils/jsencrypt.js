56c274793bcb3031b1da9be2d974dccf18487bd6
yxx7318 <1303490776@qq.com>
2025-04-09 17:33:03 +0800

    2025-04-09：替换公钥和私钥

--- yxx-ui/src/utils/jsencrypt.js
+++ yxx-ui/src/utils/jsencrypt.js
@@ -2,17 +2,17 @@ import JSEncrypt from 'jsencrypt/bin/jsencrypt.min'
 
 // 密钥对生成 http://web.chacuo.net/netrsakeypair
 
-const publicKey = 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKoR8mX0rGKLqzcWmOzbfj64K8ZIgOdH\n' +
-  'nzkXSOVOZbFu/TJhZ7rFAN+eaGkl3C4buccQd/EjEsj9ir7ijT7h96MCAwEAAQ=='
+const publicKey = 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAK2Sn4AvAbSvnsphK+h4bQdxkcM7s9xU\n' +
+  '7XB2EoRgAwxAW9ASjSfHkFQqktNFIZAJZ1r4N5MdYfvwIq8RgsDG85MCAwEAAQ=='
 
-const privateKey = 'MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAqhHyZfSsYourNxaY\n' +
-  '7Nt+PrgrxkiA50efORdI5U5lsW79MmFnusUA355oaSXcLhu5xxB38SMSyP2KvuKN\n' +
-  'PuH3owIDAQABAkAfoiLyL+Z4lf4Myxk6xUDgLaWGximj20CUf+5BKKnlrK+Ed8gA\n' +
-  'kM0HqoTt2UZwA5E2MzS4EI2gjfQhz5X28uqxAiEA3wNFxfrCZlSZHb0gn2zDpWow\n' +
-  'cSxQAgiCstxGUoOqlW8CIQDDOerGKH5OmCJ4Z21v+F25WaHYPxCFMvwxpcw99Ecv\n' +
-  'DQIgIdhDTIqD2jfYjPTY8Jj3EDGPbH2HHuffvflECt3Ek60CIQCFRlCkHpi7hthh\n' +
-  'YhovyloRYsM+IS9h/0BzlEAuO0ktMQIgSPT3aFAgJYwKpqRYKlLDVcflZFCKY7u3\n' +
-  'UP8iWi1Qw0Y='
+const privateKey = 'MIIBUwIBADANBgkqhkiG9w0BAQEFAASCAT0wggE5AgEAAkEArZKfgC8BtK+eymEr\n' +
+  '6HhtB3GRwzuz3FTtcHYShGADDEBb0BKNJ8eQVCqS00UhkAlnWvg3kx1h+/AirxGC\n' +
+  'wMbzkwIDAQABAkAJQaN964PUxfTa6Bm2WQ2/cuAKIFn0KxdKgxFzu8cN1R5CJCrS\n' +
+  'IVU4bMuFP3SS+D4kMCUjxF9kh/zhYNuri5bBAiEA5GOASb9VdGCyy7da9/SXmfBP\n' +
+  '1U8UWPrvlz73d8XMObMCIQDCjpmqAYWRPO8aGGaMwNBuFw+2SX3HrJfYByg/1eWu\n' +
+  'oQIgfzrC89Jn644W7I+3WJxke/g4sQ79udgI8su8FngVHisCIDBRYfBKuHU9gNQm\n' +
+  'QzGpoRn2ODy5O7efMYbBIEnaQVSBAiA0BSaimpB37IszYy9Vb9LuE3yuZvXNGc3R\n' +
+  'vZI8/zNayA=='
 
 // 加密
 export function encrypt(txt) {
