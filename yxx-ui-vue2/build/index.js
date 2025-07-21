07202b5956e6678175c9b515090036ae60a3ee64
yxx7318 <1303490776@qq.com>
2025-07-21 12:00:01 +0800

    2025-07-19：优化preview命令

--- yxx-ui-vue2/build/index.js
+++ yxx-ui-vue2/build/index.js
@@ -1,12 +1,12 @@
 const { run } = require('runjs')
 const chalk = require('chalk')
-const config = require('../vue.config.js')
 const rawArgv = process.argv.slice(2)
 const args = rawArgv.join(' ')
 
 // 默认值
 let baseApi = "/prod-api"
 let proxyPort = 7318
+let publicPath = "/yxx-yi"
 
 // 解析命令行参数
 rawArgv.forEach(arg => {
@@ -14,11 +14,13 @@ rawArgv.forEach(arg => {
     baseApi = arg.split('=')[1]
   } else if (arg.startsWith('--proxyPort=')) {
     proxyPort = parseInt(arg.split('=')[1], 10)
+  } else if (arg.startsWith('--proxyPort=')) {
+    publicPath = arg.split('=')[1]
   }
-});
+})
 
 // 直接引入 http-proxy-middleware
-const proxy = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware')
 
 if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
   const report = rawArgv.includes('--report')
@@ -26,7 +28,6 @@ if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
   run(`vue-cli-service build ${args}`)
 
   const port = 9526
-  const publicPath = config.publicPath
 
   let connect = require('connect')
   let serveStatic = require('serve-static')
@@ -39,6 +40,12 @@ if (process.env.npm_config_preview || rawArgv.includes('--preview')) {
     pathRewrite: {
       [`^${baseApi}`]: ''
     }
+  }))
+
+  // 添加代理中间件
+  app.use(proxy("/v3/api-docs", {
+    target: `http://localhost:${proxyPort}`,
+    changeOrigin: true,
   }))
 
   app.use(
