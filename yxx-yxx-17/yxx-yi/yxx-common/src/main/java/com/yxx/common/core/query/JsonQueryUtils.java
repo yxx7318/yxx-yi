aaa0689b464f894afc634cf168e9fc1fa71955fb
yxx7318 <1303490776@qq.com>
2025-10-16 10:36:10 +0800

    修复大小写问题

--- yxx-yxx-17/yxx-yi/yxx-common/src/main/java/com/yxx/common/core/query/JsonQueryUtils.java
+++ yxx-yxx-17/yxx-yi/yxx-common/src/main/java/com/yxx/common/core/query/JsonQueryUtils.java
@@ -1,11 +1,12 @@
-package com.yxx.common.core.utils;
+package com.yxx.common.core.query;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.yxx.common.core.utils.WrapperUtils;
 import com.yxx.common.utils.StringUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.reflection.property.PropertyNamer;
+
+import static com.yxx.common.constant.MySqlConstats.QUERY_JSON_SQL;
 
 
 /**
@@ -24,16 +25,13 @@ public class JsonQueryUtils<T> extends LambdaQueryWrapper<T> {
      * @return 增强后的LambdaQueryWrapper
      */
     public LambdaQueryWrapper<T> queryJsonColumn(LambdaQueryWrapper<T> wrapper, String column, String query, String key, Object value) {
-        if (StringUtils.isBlank(value.toString())) {
+        if (StringUtils.isNull(value)) {
             return wrapper; // 空值直接跳过
         }
 
         // 安全过滤：仅当列是有效JSON时才应用条件
         String safeCondition = String.format(
-                "CASE WHEN JSON_VALID(%s)" +
-                        "THEN JSON_UNQUOTE(JSON_EXTRACT(%s, '$.%s')) " +
-                        "ELSE NULL " +
-                        "END %s",
+                QUERY_JSON_SQL,
                 column, column, query, key
         );
         return wrapper.apply(true, safeCondition, value);
@@ -49,7 +47,7 @@ public class JsonQueryUtils<T> extends LambdaQueryWrapper<T> {
      * @return 增强后的LambdaQueryWrapper
      */
     public static <C> LambdaQueryWrapper<C> eq(LambdaQueryWrapper<C> wrapper, SFunction<C, ?> column, String key, Object value) {
-        return new JsonQueryUtils<C>().queryJsonColumn(wrapper, getDBColumn(column), key, "= {0}", value);
+        return new JsonQueryUtils<C>().queryJsonColumn(wrapper, WrapperUtils.getDBColumn(column), key, "= {0}", value);
     }
 
     /**
@@ -62,7 +60,7 @@ public class JsonQueryUtils<T> extends LambdaQueryWrapper<T> {
      * @return 增强后的LambdaQueryWrapper
      */
     public static <C, K> LambdaQueryWrapper<C> eq(LambdaQueryWrapper<C> wrapper, SFunction<C, ?> column, SFunction<K, ?> key, Object value) {
-        return new JsonQueryUtils<C>().queryJsonColumn(wrapper, getDBColumn(column), getJavaField(key), "= {0}", value);
+        return new JsonQueryUtils<C>().queryJsonColumn(wrapper, WrapperUtils.getDBColumn(column), WrapperUtils.getJavaField(key), "= {0}", value);
     }
 
     /**
@@ -75,7 +73,7 @@ public class JsonQueryUtils<T> extends LambdaQueryWrapper<T> {
      * @return 增强后的LambdaQueryWrapper
      */
     public static <C> LambdaQueryWrapper<C> like(LambdaQueryWrapper<C> wrapper, SFunction<C, ?> column, String key, Object value) {
-        return new JsonQueryUtils<C>().queryJsonColumn(wrapper, getDBColumn(column), key, "LIKE CONCAT('%', {0}, '%')", value);
+        return new JsonQueryUtils<C>().queryJsonColumn(wrapper, WrapperUtils.getDBColumn(column), key, "LIKE CONCAT('%', {0}, '%')", value);
     }
 
     /**
@@ -88,17 +86,6 @@ public class JsonQueryUtils<T> extends LambdaQueryWrapper<T> {
      * @return 增强后的LambdaQueryWrapper
      */
     public static <C, K> LambdaQueryWrapper<C> like(LambdaQueryWrapper<C> wrapper, SFunction<C, ?> column, SFunction<K, ?> key, Object value) {
-        return new JsonQueryUtils<C>().queryJsonColumn(wrapper, getDBColumn(column), getJavaField(key), "LIKE CONCAT('%', {0}, '%')", value);
-    }
-
-    /**
-     * 静态方法，通过方法引用获取到对应的MyBatisPlus映射到的数据库字段名
-     */
-    public static <C> String getDBColumn(SFunction<C, ?> column) {
-        return new JsonQueryUtils<C>().columnToString(column, true);
-    }
-
-    private static <K> String getJavaField(SFunction<K, ?> key) {
-        return PropertyNamer.methodToProperty(LambdaUtils.extract(key).getImplMethodName());
+        return new JsonQueryUtils<C>().queryJsonColumn(wrapper, WrapperUtils.getDBColumn(column), WrapperUtils.getJavaField(key), "LIKE CONCAT('%', {0}, '%')", value);
     }
 }
