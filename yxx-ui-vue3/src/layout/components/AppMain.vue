eb5e35325e370191d28c583596ffcd20118334a4
yxx7318 <1303490776@qq.com>
2025-11-03 17:01:40 +0800

    优化注释

--- yxx-ui-vue3/src/layout/components/AppMain.vue
+++ yxx-ui-vue3/src/layout/components/AppMain.vue
@@ -37,7 +37,7 @@ function addIframe() {
 
 <style lang="scss" scoped>
 .app-main {
-  /* 顶部标签导航栏 */
+  /* 去掉顶部标签导航栏 */
   height: calc(100% - 84px);
   /* 50= navbar  50  */
   min-height: calc(100vh - 50px);
