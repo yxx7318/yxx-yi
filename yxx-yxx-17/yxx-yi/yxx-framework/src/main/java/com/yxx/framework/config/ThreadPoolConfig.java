c9ad822293704ed87ffc61a1e4f018c26724e708
yxx7318 <1303490776@qq.com>
2026-03-17 15:13:18 +0800

    优化线程池配置类细节

--- yxx-yxx-17/yxx-yi/yxx-framework/src/main/java/com/yxx/framework/config/ThreadPoolConfig.java
+++ yxx-yxx-17/yxx-yi/yxx-framework/src/main/java/com/yxx/framework/config/ThreadPoolConfig.java
@@ -11,6 +11,7 @@ import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -46,6 +47,7 @@ public class ThreadPoolConfig implements AsyncConfigurer
      * Async注解使用此线程池
      */
     @Bean(name = "threadPoolTaskExecutor")
+    @Primary
     public ThreadPoolTaskExecutor threadPoolTaskExecutor()
     {
         ThreadPoolMDCWrapper executor = new ThreadPoolMDCWrapper();
